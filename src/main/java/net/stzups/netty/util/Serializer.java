56cc228704e145abd8d123a9ff6d2a16161c3fd5
stzups <griffinht@gmail.com>
2021-09-10 18:20:29 -0400

    add extends

--- src/main/java/net/stzups/netty/util/Serializer.java
+++ src/main/java/net/stzups/netty/util/Serializer.java
@@ -2,6 +2,6 @@ package net.stzups.netty.util;
 
 import io.netty.buffer.ByteBuf;
 
-public interface Serializer<T> {
+public interface Serializer<T> extends Serializable {
     void serialize(ByteBuf byteBuf, T object);
 }
